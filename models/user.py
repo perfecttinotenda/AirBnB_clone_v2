5a39935e1ce2ba8036a39707ababb5d7905d49f6
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-07-15 19:06:10 +0200

    recomment

--- models/user.py
+++ models/user.py
@@ -4,8 +4,11 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """ User class that inherits BaseModel """
+    """ User class that manages user objects """
     email = ""
     password = ""
     first_name = ""
     last_name = ""
+
+
+ 

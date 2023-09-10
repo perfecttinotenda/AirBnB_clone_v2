8cc22b4e471c861a3953a6562d50a7f8b9fced93
perfecttinotenda <perfecttinotendamashingaidze@gmail.com>
2023-09-10 12:18:51 -0700

    all tested files

---
+++ 100-clean_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# del out-of-date archives.
+import os
+from fabric.api import *
+
+env.hosts = ["35.153.231.89", "54.160.120.149"]
+
+
+def do_clean(number=0):
+    """Delete out-of-date archives.
+    """
+    number = 1 if int(number) == 0 else int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
